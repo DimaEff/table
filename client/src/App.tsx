988b52695937763fe0175e0833bf609954a0bda8
Dima Fominenkov <69797190+DimaEff@users.noreply.github.com>
2022-07-14 17:45:08 +0300

    added eslint and prettier settings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-
-import './main.scss';
+import React from "react";
+import "./main.scss";
 
 function App() {
-  return (
-    <div className={'test'}>
-      123123
-    </div>
-  );
+    return (
+        <div className={"test"}>
+            123123
+        </div>
+    );
 }
 
 export default App;
